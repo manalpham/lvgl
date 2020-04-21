37c1140cb328e2b9e754b83c6fa1d66bd947f900
Emmanuel Bernet-Rollande <aeroman@alpham.eu>
2020-04-21 16:14:07 +0200

    add gray4 theme include in lv_theme.h

--- src/lv_themes/lv_theme.h
+++ src/lv_themes/lv_theme.h
@@ -372,6 +372,7 @@ lv_theme_t * lv_theme_get_current(void);
 #include "lv_theme_night.h"
 #include "lv_theme_zen.h"
 #include "lv_theme_mono.h"
+#include "lv_theme_gray4.h"
 #include "lv_theme_nemo.h"
 #include "lv_theme_material.h"
 
